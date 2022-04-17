aae4a743bb73310e69388141b610f25b663bcb65
barisssssss <103127199+barisssssss@users.noreply.github.com>
2022-04-17 19:03:41 +0300

    abc
    abc

--- cvad_hw1/models/cilrs.py
+++ cvad_hw1/models/cilrs.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 class CILRS(nn.Module):
     """An imitation learning agent with a resnet backbone."""
     def __init__(self):
-        super(CarlaNet,self).__init__()
+        
 
     def forward(self, img, command):
         pass
